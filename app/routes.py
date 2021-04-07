040c27bfaa31e7fd0708d051919d123aebeddb10
Moshe Shadmon <moshe@anylog.co>
2021-04-07 16:14:21 -0700

    forms

--- app/routes.py
+++ app/routes.py
@@ -35,4 +35,4 @@ def index():
 @app.route('/login')
 def login():
     form = LoginForm()
-    return render_template('login.html', title = 'Sighn In', form = form)
+    return render_template('login.html', title = 'Sign In', form = form)
