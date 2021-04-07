f2f1217e35a51e373156ff7eb685475cef5bc4b2
Moshe Shadmon <moshe@anylog.co>
2021-04-07 11:26:48 -0700

    clean branch

--- react-spa/src/components/NotFound.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const NotFound: React.FC = () => <>404</>;
-
-export default NotFound;
--- react-spa/src/lib/isDev.ts
+++
@@ -1,4 +0,0 @@
-import process from 'process';
-
-const isDev = !process.env.NODE_ENV || ['development', 'test'].includes(process.env.NODE_ENV);
-export default isDev;
