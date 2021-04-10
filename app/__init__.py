e17a621573cdb81834e831ed81cece5bfc7f4816
Moshe Shadmon <moshe@anylog.co>
2021-04-10 11:13:28 -0700

    configure form

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,5 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 from app import routes          # Routes includes the implemented URLs
+
+
---
+++ app/app_view.py
@@ -0,0 +1,13 @@
+
+from config import Config
+
+# ------------------------------------------------------------------------
+# The process to load a JSON file that maintanins the GUI view of the data/metadata
+# ------------------------------------------------------------------------
+
+def load_json():
+
+    file_name = Config.GUI_VIEW
+    pass
+
+
