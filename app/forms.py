2b92e33ebc6ee8de527dbbf80037c7e342e90979
Moshe Shadmon <moshe@anylog.co>
2021-05-05 12:14:15 -0700

    Configure

--- app/forms.py
+++ app/forms.py
@@ -23,12 +23,8 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Sign In')
 
 class ConfigForm(FlaskForm):
-    company = StringField('Company', validators=[DataRequired()])
     node_ip = StringField('Node IP', validators=[DataRequired()])
     node_port = IntegerField('Node Port', validators=[DataRequired()])
-    reports_ip = StringField('Reports IP', validators=[DataRequired()])
-    reports_port = IntegerField('Reports Port', validators=[DataRequired()])
-
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Save')
 
